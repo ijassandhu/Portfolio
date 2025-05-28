6a5bda619346bfb3a4b6346beb0b562b95b73b32
Jaskeerat <jaskeeratsingh219@gmail.com>
2025-05-29 01:28:59 +0530

    testing

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 require("dotenv").config();
 
 const Message = require("./models/Message");
