670fc5b73ed784d24ede4ca106d6d5f8f8895622
Jaskeerat <jaskeeratsingh219@gmail.com>
2025-05-28 22:55:37 +0530

    c

---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const Message = require("./models/Message");
+
+const app = express();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "ijassandhu.vercel.app",
+    methods: ["POST", "GET"],
+  })
+);
+app.use(express.json());
+const PORT = process.env.PORT;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"));
+
+app.post("/api/contact", async (req, res) => {
+  const { name, email, message } = req.body;
+  try {
+    await Message.create({ name, email, message });
+    res.status(201).json({ success: true });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+app.get("/api/messages", async (req, res) => {
+  const messages = await Message.find().sort({ timestamp: -1 });
+  res.json(messages);
+});
+
+// Start server
+app.listen(5000, () => {
+  console.log(`Server running on ${PORT}`);
+});
